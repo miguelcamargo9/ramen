aa99bccde93196a0420190b3ababa61a4443a553
miguelcamargo9 <miguelcamargo9@gmail.com>
2014-06-04 01:30:41 +0000

    Ajustes para el registro de orden!

--- web/controladores/facturaControlador.php
+++ web/controladores/facturaControlador.php
@@ -1,7 +1,16 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace controladores;
+require_once '../../persistencia/sesion/facturasFacade.php';
+use sesion\facturasFacade as factura;
+$facade = new factura();
+$idFactura = $facade->guardarFactura($_POST['cliente'], $_POST['totalProductos']);
+for ($index = 0;$index < count($_POST);$index++) {
+  if($_POST['productos'.$index] != ''){
+    $idFacturaPro = $facade->guardarFacturaProducto($idFactura, $_POST['productos'.$index], $_POST['cantidad_'.$_POST['productos'.$index]]);
+  }
+}
+
+//$_SESSION['exito'] = "Producto guardado con exito";
+//header("../vistas/inventario/crearProducto.php");
 ?>
